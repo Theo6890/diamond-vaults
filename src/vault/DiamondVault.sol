8c836eeae0b0615200640978e7ccd360a580e170
Théo RICHARD <mail@rcode.tech>
2022-12-09 11:14:26 +0100

    feat: diamond vault NatSpec and required vars

---
+++ src/vault/DiamondVault.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/**
+ * @notice A diamond contains one or more facets that implement the logic of the app.
+ *
+ *      The diamond only contains immutable functions, which means these functions will
+ *      never be updated at any point in time.
+ *
+ *      The diamond serves as a storage to save all data from the facets
+ *
+ *
+ * @dev As we use a delegatecall, `msg.sender` & `msg.value` will always depend on the
+ *      diamond's context.
+ *      In other words, if walletA is `msg.sender` & 10 ether is `msg.value` in the
+ *      diamond, the same values will be used in called facets.
+ *
+ *      The diamond maps function's selector/signature to the address that implements it.
+ *      Unfortunately, in the `EIP-2535` a diamond can only implement a function once. We
+ *      can not use the same function selector/signature in different facets, otherwise
+ *      the mapping will fail.
+ *
+ */
+contract DiamondVault {
+    address public proxyOwner;
+
+    // @return the address where the given function selector is implemented
+    mapping(bytes4 => address) public implementationOf;
+
+    enum ACTION {
+        ADD,
+        UPDATE,
+        REMOVE
+    }
+}
