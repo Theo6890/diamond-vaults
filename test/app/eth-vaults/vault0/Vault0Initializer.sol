dc21010a7fbcfac14bc352df4c202c5c8d59545c
Théo RICHARD <mail@rcode.tech>
2022-12-23 16:08:05 +0100

    refactor(test): helper file to Vault0 tests

---
+++ test/app/eth-vaults/vault0/Vault0Initializer.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import {DiamondInitiliazerHelper} from "../../../helpers/DiamondInitiliazerHelper.sol";
+
+import {Vault0Facet} from "../../../../src/app/eth-vaults/vault0/Vault0Facet.sol";
+
+contract Vault0Initializer is DiamondInitiliazerHelper {
+    Vault0Facet vault0;
+
+    function setUp() public {
+        _instanciateFacets();
+        _registerFacetAddressesAndNames();
+        _createAllFacetCut();
+        _updateDiamondWithNewFacets();
+    }
+
+    function _instanciateFacets() internal override {
+        super._instanciateFacets();
+        // deploy Vault0Facet
+        vault0 = new Vault0Facet();
+        emit log_named_address("vault0 addr", address(vault0));
+    }
+
+    function _registerFacetAddressesAndNames() internal override {
+        super._registerFacetAddressesAndNames();
+        facetsAddress.push(address(vault0));
+        facetsName.push("Vault0Facet");
+    }
+}
